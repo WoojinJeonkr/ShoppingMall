84f9743dac77875fb183a32fd9204937fd8c0443
Yusubin <yusubin288@gmail.com>
2022-04-11 15:39:31 +0900

    [유수빈]수정

--- clothingShoppingMall/src/main/java/com/hi/clothingStore/vo/ServiceCenterVO.java
+++ clothingShoppingMall/src/main/java/com/hi/clothingStore/vo/ServiceCenterVO.java
@@ -1,48 +1,82 @@
 package com.hi.clothingStore.vo;
 
+import java.sql.Timestamp;
+
 public class ServiceCenterVO {
 
-	private int serviceIdx;
-	private String serviceWriter;
-	private String serviceTitle;
-	private String serviceQuestion;
-	private String serviceRgstday;
+
+	private int service_idx;
+	private String service_writer;
+	private String service_title;
+	private String service_question;
+	private Timestamp service_rgstday;
+	private int service_idx_re ;
+	private int service_id_origin ;
+	private int service_reply_group;
+	private int service_notice;
 	
-	public int getServiceIdx() {
-		return serviceIdx;
-	}
-	public void setServiceIdx(int serviceIdx) {
-		this.serviceIdx = serviceIdx;
-	}
-	public String getServiceWriter() {
-		return serviceWriter;
-	}
-	public void setServiceWriter(String serviceWriter) {
-		this.serviceWriter = serviceWriter;
-	}
-	public String getServiceTitle() {
-		return serviceTitle;
-	}
-	public void setServiceTitle(String serviceTitle) {
-		this.serviceTitle = serviceTitle;
-	}
-	public String getServiceQuestion() {
-		return serviceQuestion;
-	}
-	public void setServiceQuestion(String serviceQuestion) {
-		this.serviceQuestion = serviceQuestion;
-	}
-	public String getServiceRgstday() {
-		return serviceRgstday;
-	}
-	public void setServiceRgstday(String serviceRgstday) {
-		this.serviceRgstday = serviceRgstday;
-	}
 	
+	public int getService_idx() {
+		return service_idx;
+	}
+	public void setService_idx(int service_idx) {
+		this.service_idx = service_idx;
+	}
+	public String getService_writer() {
+		return service_writer;
+	}
+	public void setService_writer(String service_writer) {
+		this.service_writer = service_writer;
+	}
+	public String getService_title() {
+		return service_title;
+	}
+	public void setService_title(String service_title) {
+		this.service_title = service_title;
+	}
+	public String getService_question() {
+		return service_question;
+	}
+	public void setService_question(String service_question) {
+		this.service_question = service_question;
+	}
+	public Timestamp getService_rgstday() {
+		return service_rgstday;
+	}
+	public void setService_rgstday(Timestamp service_rgstday) {
+		this.service_rgstday = service_rgstday;
+	}
+	public int getService_idx_re() {
+		return service_idx_re;
+	}
+	public void setService_idx_re(int service_idx_re) {
+		this.service_idx_re = service_idx_re;
+	}
+	public int getService_id_origin() {
+		return service_id_origin;
+	}
+	public void setService_id_origin(int service_id_origin) {
+		this.service_id_origin = service_id_origin;
+	}
+	public int getService_reply_group() {
+		return service_reply_group;
+	}
+	public void setService_reply_group(int service_reply_group) {
+		this.service_reply_group = service_reply_group;
+	}
+	public int getService_notice() {
+		return service_notice;
+	}
+	public void setService_notice(int service_notice) {
+		this.service_notice = service_notice;
+	}
 	@Override
 	public String toString() {
-		return "ServicecenterVO [serviceIdx=" + serviceIdx + ", serviceWriter=" + serviceWriter + ", serviceTitle="
-				+ serviceTitle + ", serviceQuestion=" + serviceQuestion + ", serviceRgstday=" + serviceRgstday + "]";
+		return "ServiceCenterVO [service_idx=" + service_idx + ", service_writer=" + service_writer + ", service_title="
+				+ service_title + ", service_question=" + service_question + ", service_rgstday=" + service_rgstday
+				+ ", service_idx_re=" + service_idx_re + ", service_id_origin=" + service_id_origin
+				+ ", service_reply_group=" + service_reply_group + ", service_notice=" + service_notice + "]";
 	}
-
+	
+	
 }
